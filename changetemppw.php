33d6ddabb882b6878e9df2c9799a8bdee5905e71
Michael Brown <michaelbrown.tsbod@gmail.com>
2017-03-22 09:01:30 -0400

    Update changetemppw.php

--- changetemppw.php
+++ changetemppw.php
@@ -1,11 +1,9 @@
 <?php
 	session_start();
-	echo "SESSION ARRAY"<br />";
 	echo '<pre>';
 	var_dump($_SESSION);
 	echo '</pre>';
-
-	echo "POST ARRAY <br />";
+	echo '<br />';
 	echo "<pre>";
 	var_dump($_POST);
 	echo "<pre>";
