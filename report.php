95e5120452c835725403b3e441a9189de339602c
mikeabrown09 <mike.a.brown09@gmail.com>
2017-03-29 12:23:35 -0400

    Update report.php

--- report.php
+++ report.php
@@ -11,129 +11,89 @@ header('location: index.php');
 <title>Report for Motion</title>
 </head>
 <body>
-	<?php
-		$userid=$_SESSION['user_id'];
-		$motionid=$_POST['motionid'];
-
+<?php
+	$userid=$_SESSION['user_id'];
+	$motionid=$_POST['motionid'];
 		include_once ('include/db-config.php');
-		
-		//Temporary, for testing right now
-	        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-		$motion=$db_con->prepare ("SELECT * from motions where motion_id = :motionid");
-		$motion->bindParam(':motionid',$motionid);
-		$motion->execute();
-		while ($row=$motion->fetch(PDO::FETCH_ASSOC))
-		{
-			$motionid=$row['motion_id'];
-			$motionname=$row['motion_name'];
-			$dateadded=$row['dateadded'];
-			$motiondesc=$row['motion_description'];
-			$disposition=$row['motion_disposition'];
-		
-			echo "<h1>" . $motionname . "</h1>";
-			echo "<h2>Date Added:</h2>" . $dateadded . "<br />";
-			echo "<h2>Motion Text</h2>";
-			echo $motiondesc;
-			echo "<h2>Disposition:</h2>";
-			echo $disposition;
+			$motion=$db_con->prepare ("SELECT * from motions where motion_id = :motionid");
+			$motion->bindParam(':motionid',$motionid);
+			$motion->execute();
+			while ($row=$motion->fetch(PDO::FETCH_ASSOC))
+			{
+				$motionid=$row['motion_id'];
+				$motionname=$row['motion_name'];
+				$dateadded=$row['dateadded'];
+				$motiondesc=$row['motion_description'];
+				$disposition=$row['motion_disposition'];
 			
-		}//end of while ($row=$motion->fetch(PDO::FETCH_ASSOC))
-		
-	?>
-	
-	<br /><br />
-	<h2>Current Votes</h2>
-	<table border="1" width="100%">
-		<tr>
-			<th>User</th>
-			<th>Date</th>
-				<th>Vote</th>
-		</tr>
+				echo "<h1>" . $motionname . "</h1>";
+				echo "<h2>Date Added:</h2>" . $dateadded . "<br />";
+				echo "<h2>Motion Text</h2>";
+				echo $motiondesc;
+				echo "<h2>Disposition:</h2>";
+				echo $disposition;
+				
+			}//end of while ($row=$motion->fetch(PDO::FETCH_ASSOC))
+			$motion->closeCursor();
+			?>
+			<br /><br />
+			<h2>Current Votes</h2>
+			<table border="1" width="100%">
+				<tr>
+					<th>User</th>
+					<th>Date</th>
+					<th>Vote</th>
+				</tr>
+				<?php
+					$votes=$db_con->prepare(
+						"SELECT u.first_name, u.last_name, v.time, v.vote from votes v inner join motions m on m.motion_id=v.motions_id INNER join users u on u.users_id=v.users_id where m.motion_id=:motionid ORDER BY v.time ASC;");
+					$votes->bindParam(':motionid',$motionid);
+					$votes->execute();
+					while ($row=$votes->fetch(PDO::FETCH_ASSOC))
+					{
+						$firstname=$row['first_name'];
+						$lastname=$row['last_name'];
+						$votetime=$row['time'];
+						$votecast=$row['vote'];
+						echo "<tr>";
+							echo "<td>" . $firstname . " " . $lastname . "</td>";
+							echo "<td>" . $votetime . "</td>";
+							echo "<td>" . $votecast . "</td>";
+						echo "</tr>";
+					}// while ($row=$votes->fetch(PDO::FETCH_ASSOC))
+					$votes->closeCursor();
+					echo "</table>";
+				?>
 
-		<?php
-			$votes=$db_con->prepare(
-				"SELECT u.first_name, u.last_name, v.time, v.vote from votes v inner join 
-				motions m on m.motion_id=v.motions_id INNER join users u on u.users_id=v.users_id 
-				where m.motion_id=:motionid ORDER BY v.time ASC;");
-			$votes->bindParam(':motionid',$motionid);
-			$votes->execute();
-			while ($row=$votes->fetch(PDO::FETCH_ASSOC))
-			{
-				$firstname=$row['first_name'];
-				$lastname=$row['last_name'];
-				$votetime=$row['time'];
-				$votecast=$row['vote'];
-				echo "<tr>";
-					echo "<td>" . $firstname . " " . $lastname . "</td>";
-					echo "<td>" . $votetime . "</td>";
-					echo "<td>" . $votecast . "</td>";
-				echo "</tr>";
-			}// while ($row=$votes->fetch(PDO::FETCH_ASSOC))
+		<br /><br />
+		<h2>Discussions</h2>
+		<table border="1" width="100%">
+			<tr>
+				<th>User</th>
+				<th>Date</th>
+				<th>Comment</th>
+			</tr>
+			<?php
+				$motiondiscussions=$db_con->prepare(
+					"SELECT u.first_name,u.last_name,d.dateadded,d.discussion_text from users u inner join discussion d on d.user_id=u.users_id where d.motion_id=:motionid");
+				$motiondiscussions->bindParam(':motionid',$motionid);
+				$motiondiscussions->execute();
+				while ($row=$motiondiscussions->fetch(PDO::FETCH_ASSOC))
+				{
+					$firstname=$row['first_name'];
+					$lastname=$row['last_name'];
+					$discussiontime=$row['dateadded'];
+					$discussiontext=$row['discussion_text'];
+					echo "<tr>";
+						echo "<td>" . $firstname . " " . $lastname . "</td>";
+						echo "<td>" . $discussiontime . "</td>";
+						echo "<td>" . $discussiontext . "</td>";
+					echo "</tr>";
+				}//end of while
+			$motiondiscussions->closeCursor();
+				echo "</table>";
+			?>
 
-			echo "</table>";
-		?>
-
-	<br /><br />
-	<h2>Discussions</h2>
-	<table border="1" width="100%">
-		<tr>
-			<th>User</th>
-			<th>Date</th>
-			<th>Comment</th>
-		</tr>
-		<?php
-			$motiondiscussions=$db_con->prepare(
-				"SELECT u.first_name,u.last_name,d.dateadded,d.discussion_text from users u inner 
-				join discussion d on d.user_id=u.users_id where d.motion_id=:motionid");
-			$motiondiscussions->bindParam(':motionid',$motionid);
-			$motiondiscussions->execute();
-			while ($row=$motiondiscussions->fetch(PDO::FETCH_ASSOC))
-			{
-				$firstname=$row['first_name'];
-				$lastname=$row['last_name'];
-				$discussiontime=$row['dateadded'];
-				$discussiontext=$row['discussion_text'];
-				echo "<tr>";
-					echo "<td>" . $firstname . " " . $lastname . "</td>";
-					echo "<td>" . $discussiontime . "</td>";
-					echo "<td>" . $discussiontext . "</td>";
-				echo "</tr>";
-			}//end of while
-		?>
-	</table>
-	<h2>Change Log</h2>
-	<table border="1" width="100%">
-		<tr>
-			<th>User</th>
-			<th>Field</th>
-			<th>Old Value</th>
-			<th>New Value</th>
-		</tr>
-		<?php
-			$changeLogQuery=$db_con->prepare("SELECT u.first_name,u.last_name,mcl.field,mcl.oldValue,mcl.newValue FROM users u inner join motionChangeLog mcl on mcl.userid=u.users_id  WHERE mcl.motionid=:motionid;");
-			$changeLogQuery->bindParam(':motionid',$motionid);
-			$changeLogQuery->execute();
-			$results=$changeLogQuery->fetchAll();
-			foreach ($results as $row)
-			{
-				print_r( $row );
-			}
-			//while ($row=$changeLogQuery->fetch(PDO::FETCH_ASSOC))
-			//{
-			//	$firstname=$row['first_name'];
-			//	$lastname=$row['last_name'];
-			//	$field=$row['field'];
-			//	$oldValue=$row['oldValue'];
-			//	$newValue=$row['newValue'];
-			//	echo "<tr>";
-			//		echo "<td>" . $firstname . " " . $lastname . "</td>";
-			//		echo "<td>" . $field . "</td>";
-			//		echo "<td>" . $oldValue . "</td>";
-			//		echo "<td>" . $newValue . "</td>";
-			//	echo "</td>";
-			//}
-		?>
-	</table>
+ 
 </body>
 </html>
