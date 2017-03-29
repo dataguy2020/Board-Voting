351fbe1dd2b522746af9d7c28489ec73143bea61
Michael Brown <michaelbrown.tsbod@gmail.com>
2017-03-28 22:50:08 -0400

    updated

--- amendMotion.php
+++ amendMotion.php
@@ -1,12 +1,32 @@
-<?php
-	$motionid=$_POST['motionid'];
-	$motiontext=$_POST['motiondesc'];
-	include_once('include/db-config.php');
-
-	if ($motiontext == "")
-	{
-		
-	}
-
+<html>
+	<head>
+		<title>Amending Motion</title>
+	</head>
+	<body>
+		<?php
+		$motionid=$_POST['motionid'];
+		$motiontext=$_POST['motiondesc'];
+		include_once('include/db-config.php');
 	
-?>
+		if ($motiontext != "")
+		{
+			echo "You did not enter anything";
+			$updateMotion=$db_con->prepare(
+					"UPDATE motions SET motion_description=:description where motions_id=:motionid;");
+			$updateMotion->execute(array(':updatedvote'=>$motiontext,':motionid' =>$motionid));
+			echo "Updated the motion";
+			echo "<br />";
+			$dispo="AMENDED";
+			$amendMotion=$db_con->prepare(
+					"UPDATE motions SET motion_disposition=:dispo where motions_id=:motionid;");
+			$amendMotion->execute(array(':dispo'=>$dispo,':motionid'=>$motionid));
+			echo "Updated motion disposition";
+		}	
+		else
+		{
+			echo "You did not enter anything";
+		}
+		?>
+		<a href="dashboard.php">Main Dashboard</a>
+	</body>
+</html>
