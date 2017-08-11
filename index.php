b33d0d1b15829ee58687bbabd6392dd78fce755b
Michael Brown <michaelbrown.tsbod@gmail.com>
2017-08-11 11:12:35 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,45 +40,6 @@ vertical-align: middle;
 
 <body>
 	
-	<div class="navbar navbar-fixed-top">
-	
-	<div class="navbar-inner">
-		
-		<div class="container">
-			
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
-			
-			<a class="brand" href="index.php">
-				Tanyard Springs Board Portal
-			</a>		
-			
-			<div class="nav-collapse">
-				<ul class="nav pull-right">
-					
-					<!--<li class="">						
-						<a href="signup.php" class="">
-							Don't have an account?
-						</a>
-						
-					</li>-->
-					
-				
-				</ul>
-				
-			</div><!--/.nav-collapse -->	
-	
-		</div> <!-- /container -->
-		
-	</div> <!-- /navbar-inner -->
-	
-</div> <!-- /navbar -->
-
-
-
 <div class="account-container">
 	
 	<div class="content clearfix">
