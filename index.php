92b78f9a4abeb7cf0acd3af1b15072bc8b0b1e19
mikeabrown09 <mike.a.brown09@gmail.com>
2017-09-26 14:09:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -36,6 +36,8 @@ vertical-align: middle;
 	  }
 	  
 </style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -84,7 +86,9 @@ vertical-align: middle;
 	</div> <!-- /content -->
 	
 </div> <!-- /account-container -->
-
+      <div class="alert alert-danger" role="alert">
+  		<strong>MESSAGE:</strong>This system will be going offline starting at 9:00p.m. on Friday September 29, 2017 until 12:00 a.m. on Saturday September 30, 2017
+	      </div>
 
 
 
