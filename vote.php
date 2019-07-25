3d0815120674d15b054f68882a6dbb1119c9cf95
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-07-25 19:49:59 -0400

    Update vote.php

--- vote.php
+++ vote.php
@@ -643,7 +643,7 @@ table {
 <!-- Placed at the end of the document so the pages load faster --> 
 <script src="js/jquery-1.7.2.min.js"></script> 
 
-<script src="js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
  
 <?php } ?>
 </body>
