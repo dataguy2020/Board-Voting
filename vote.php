f2d43d39cb613b48a711977014c47e4e71c7faf2
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-07-25 19:55:17 -0400

    Update vote.php

--- vote.php
+++ vote.php
@@ -28,9 +28,13 @@ table {
 	width:"100%";
 }
 </style>
+	<script>
 	$(document).ready(function() {
     $('#openMotions').DataTable();
 } );
+	</script>
+	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 </head>
 <body>
 <?php
@@ -641,8 +645,7 @@ table {
 <!-- Le javascript
 ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
-			<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+			
  
 <?php } ?>
 </body>
