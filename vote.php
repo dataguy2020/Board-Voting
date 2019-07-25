c210945f9bfa06f021b2ec6e46c4c6d6bd520443
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-07-25 19:50:47 -0400

    Update vote.php

--- vote.php
+++ vote.php
@@ -641,8 +641,7 @@ table {
 <!-- Le javascript
 ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
-<script src="js/jquery-1.7.2.min.js"></script> 
-
+			<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
  
 <?php } ?>
