ddcd3347f9e333f71c9403a4f13d5418262e2dd6
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-07-25 22:46:08 -0400

    Update discussions.php

--- discussions.php
+++ discussions.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-if(empty($_SESSION['user_id'])){
-header('location: index.php');	
+if (empty($_SESSION['user_id'])) {
+    header('location: index.php');
 }
 ?>
 <!DOCTYPE html>
@@ -24,34 +24,26 @@ rel="stylesheet">
 <![endif]-->
 
 <style>
-	table {
-		width: 100%;
-}	
+    table {
+        width: 100%;
+}    
 </style>
-	<script src="https://cdn.ckeditor.com/ckeditor5/12.3.1/classic/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/12.3.1/classic/ckeditor.js"></script>
 <script>
-	$('#dt-basic-checkbox').dataTable({
-
-columnDefs: [{
-orderable: false,
-className: 'select-checkbox',
-targets: 0
-}],
-select: {
-style: 'os',
-selector: 'td:first-child'
-}
+    $(document).ready(function () {
+$('#dtBasicExample').DataTable();
+$('.dataTables_length').addClass('bs-select');
 });
-	</script>
+    </script>
 </head>
 <body>
 <?php
-$userid=$_SESSION['user_id'];
+$userid = $_SESSION['user_id'];
 ?>
 <div class="navbar navbar-fixed-top">
 <div class="navbar-inner">
 <div class="container"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span
-	    class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.php">Tanyard Springs Board Portal</a>
+        class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> </a><a class="brand" href="index.php">Tanyard Springs Board Portal</a>
 
 <!--/.nav-collapse --> 
 </div>
@@ -65,10 +57,10 @@ $userid=$_SESSION['user_id'];
 <div class="container">
 <ul class="mainnav">
 <li><a href="dashboard.php"><em class="icon-dashboard"></em><span>Dashboard</span> </a> </li>
-	<li> <a href ="add.php"><em class ="icon-dashboard"></em><span>Add Motion</span></a></li>
-	<li><a href="vote.php"><em class ="icon-dashboard"></em><span>Vote</span></a></li>
-	 <li class="active"><a href="discussions.php"><em class ="icon-dashboard"></em><span>Discussions</span></a></li>
-	<li><a href="userprefs.php"><em class ="icon-dashboard"></em><span>Prefences</span></a></li>
+    <li> <a href ="add.php"><em class ="icon-dashboard"></em><span>Add Motion</span></a></li>
+    <li><a href="vote.php"><em class ="icon-dashboard"></em><span>Vote</span></a></li>
+     <li class="active"><a href="discussions.php"><em class ="icon-dashboard"></em><span>Discussions</span></a></li>
+    <li><a href="userprefs.php"><em class ="icon-dashboard"></em><span>Prefences</span></a></li>
          <li><a href="logout.php"><em class="icon-dashboard"></em><span>Logout</span></a> </li>
 
       </ul>
@@ -82,100 +74,95 @@ $userid=$_SESSION['user_id'];
   <div class="main-inner">
     <div class="container">
       <div class="row">
-	<?php
+    <?php
 
-		include_once ('include/db-config.php');
+include_once('include/db-config.php');
 
-		if (!empty($_POST))
-		{
-			$motionid=$_POST['motionid'];
-			$motion=$db_con->prepare ("SELECT * from motions where motion_id = :motionid");
-			$motion->bindParam(':motionid',$motionid);
-			$motion->execute();
-			while ($row=$motion->fetch(PDO::FETCH_ASSOC))
-			{
-				$motionid=$row['motion_id'];
-				$motionname=$row['motion_name'];
-				$dateadded=$row['dateadded'];
-				$session=$row['Session'];
-				$motiondesc=$row['motion_description'];
-			
-				echo "<h1>" . $motionname . "</h1><br />";
-				echo "<h2>Date Added:</h2>" . $dateadded . "<br />";
-				echo "<h2>Session:</h2>" . $session . "<br /><br />";
-				echo "<h2>Motion Text</h2>";
-				echo $motiondesc;
-			}//end of while ($row=$motion->fetch(PDO::FETCH_ASSOC))
-	      ?>
-			<br /><br />
-			<h2>Current Votes</h2>
-			<table border="1">
-			<tr>
-				<th>User</th>
-				<th>Date</th>
-				<th>Vote</th>
-			</tr>
-			<?php
-				$votes=$db_con->prepare(
-					"SELECT u.first_name, u.last_name, v.time, v.vote from votes v inner join motions m on m.motion_id=v.motions_id INNER join users u on u.users_id=v.users_id where m.motion_id=:motionid");
-				$votes->bindParam(':motionid',$motionid);
-				$votes->execute();
-			while ($row=$votes->fetch(PDO::FETCH_ASSOC))
-			{
-				$firstname=$row['first_name'];
-				$lastname=$row['last_name'];
-				$votetime=$row['time'];
-				$votecast=$row['vote'];
-				echo "<tr>";
-					echo "<td>" . $firstname . " " . $lastname . "</td>";
-					echo "<td>" . $votetime . "</td>";
-					echo "<td>" . $votecast . "</td>";
-				echo "</tr>";
-			}//end of while ($row=$votes->fetch(PDO::FETCH_ASSOC))
-			echo "</table>";
-		?>
-
-		<br /><br />
-		<h2>Discussions</h2>
-		<table border="1">
-		<tr>
-			<th>User</th>
-			<th>Date</th>
-			<th>Comment</th>
-		</tr>
-		<?php
-			$motiondiscussions=$db_con->prepare(
-			"SELECT u.first_name,u.last_name,d.dateadded,d.discussion_text from users u inner join discussion d on d.user_id=u.users_id where d.motion_id=:motionid");
-			$motiondiscussions->bindParam(':motionid',$motionid);
-			$motiondiscussions->execute();
-			while ($row=$motiondiscussions->fetch(PDO::FETCH_ASSOC))
-			{
-				$firstname=$row['first_name'];
-				$lastname=$row['last_name'];
-				$discussiontime=$row['dateadded'];
-				$discussiontext=$row['discussion_text'];
-				echo "<tr>";
-					echo "<td>" . $firstname . " " . $lastname . "</td>";
-					echo "<td>" . $discussiontime . "</td>";
-					echo "<td>" . $discussiontext . "</td>";
-				echo "</tr>";
-			}//end of while
-			echo "</table>";
-
-		?>
-		 <br />
-        <br />
-	<h2>Your Discussion:</h2>
-		
-        <?php echo '<form id="discussion" name="discussion" method="POST" action="adddiscussion.php">
-                <input type="hidden" name="motionid" value="' . htmlspecialchars($motionid) . '">
-		<input type="hidden" name="userid" value="' . htmlspecialchars($userid) . '">
-		<textarea name="discussiontext" id="discussiontext" style="width:1136px; height: 122px;"></textarea>
-                <input type="Submit" name="Submit" value="Submit">
-		<input type="Reset" name="Reset" value="Reset">';
+if (!empty($_POST)) {
+    $motionid = $_POST['motionid'];
+    $motion   = $db_con->prepare("SELECT * from motions where motion_id = :motionid");
+    $motion->bindParam(':motionid', $motionid);
+    $motion->execute();
+    while ($row = $motion->fetch(PDO::FETCH_ASSOC)) {
+        $motionid   = $row['motion_id'];
+        $motionname = $row['motion_name'];
+        $dateadded  = $row['dateadded'];
+        $session    = $row['Session'];
+        $motiondesc = $row['motion_description'];
+        
+        echo "<h1>" . $motionname . "</h1><br />";
+        echo "<h2>Date Added:</h2>" . $dateadded . "<br />";
+        echo "<h2>Session:</h2>" . $session . "<br /><br />";
+        echo "<h2>Motion Text</h2>";
+        echo $motiondesc;
+    } //end of while ($row=$motion->fetch(PDO::FETCH_ASSOC))
 ?>
-		
-		<script>
+           <br /><br />
+            <h2>Current Votes</h2>
+            <table border="1">
+            <tr>
+                <th>User</th>
+                <th>Date</th>
+                <th>Vote</th>
+            </tr>
+            <?php
+    $votes = $db_con->prepare("SELECT u.first_name, u.last_name, v.time, v.vote from votes v inner join motions m on m.motion_id=v.motions_id INNER join users u on u.users_id=v.users_id where m.motion_id=:motionid");
+    $votes->bindParam(':motionid', $motionid);
+    $votes->execute();
+    while ($row = $votes->fetch(PDO::FETCH_ASSOC)) {
+        $firstname = $row['first_name'];
+        $lastname  = $row['last_name'];
+        $votetime  = $row['time'];
+        $votecast  = $row['vote'];
+        echo "<tr>";
+        echo "<td>" . $firstname . " " . $lastname . "</td>";
+        echo "<td>" . $votetime . "</td>";
+        echo "<td>" . $votecast . "</td>";
+        echo "</tr>";
+    } //end of while ($row=$votes->fetch(PDO::FETCH_ASSOC))
+    echo "</table>";
+?>
+
+        <br /><br />
+        <h2>Discussions</h2>
+        <table border="1">
+        <tr>
+            <th>User</th>
+            <th>Date</th>
+            <th>Comment</th>
+        </tr>
+        <?php
+    $motiondiscussions = $db_con->prepare("SELECT u.first_name,u.last_name,d.dateadded,d.discussion_text from users u inner join discussion d on d.user_id=u.users_id where d.motion_id=:motionid");
+    $motiondiscussions->bindParam(':motionid', $motionid);
+    $motiondiscussions->execute();
+    while ($row = $motiondiscussions->fetch(PDO::FETCH_ASSOC)) {
+        $firstname      = $row['first_name'];
+        $lastname       = $row['last_name'];
+        $discussiontime = $row['dateadded'];
+        $discussiontext = $row['discussion_text'];
+        echo "<tr>";
+        echo "<td>" . $firstname . " " . $lastname . "</td>";
+        echo "<td>" . $discussiontime . "</td>";
+        echo "<td>" . $discussiontext . "</td>";
+        echo "</tr>";
+    } //end of while
+    echo "</table>";
+    
+?>
+        <br />
+        <br />
+    <h2>Your Discussion:</h2>
+        
+        <?php
+    echo '<form id="discussion" name="discussion" method="POST" action="adddiscussion.php">
+                <input type="hidden" name="motionid" value="' . htmlspecialchars($motionid) . '">
+        <input type="hidden" name="userid" value="' . htmlspecialchars($userid) . '">
+        <textarea name="discussiontext" id="discussiontext" style="width:1136px; height: 122px;"></textarea>
+                <input type="Submit" name="Submit" value="Submit">
+        <input type="Reset" name="Reset" value="Reset">';
+?>
+       
+        <script>
     ClassicEditor
         .create( document.querySelector( '#discussiontext' ) )
         .catch( error => {
@@ -183,54 +170,60 @@ $userid=$_SESSION['user_id'];
         } );
 </script>
 
-<?php 
-			echo '
+<?php
+    echo '
         </form>';
+    
+?>
 
-	?>
+        <?php
+} //end of if statement
 
-		<?php			
-		}//end of if statement
-
-		else
-		{
-	?>
-	<p>Please choose a motion to vote on. Only one motion can be voted
-		on at a time</p>
-		<table border="1"id="dt-basic-checkbox" class="table table-striped table-bordered" cellspacing="0" width="100%>
+else {
+?>
+   <p>Please choose a motion to vote on. Only one motion can be voted
+        on at a time</p>
+        <table border="1"id="dt-basic-checkbox" class="table table-striped table-bordered" cellspacing="0" width="100%>
                 <thead>
-															  <tr>
+                                                              <tr>
                         <th>Motion ID</th>
                         <th>Motion Name</th>
                         <th>Date Added</th>
-			<th>Action</th>
+            		<th>Action</th>
                 </tr>
-															  </thead>
+                                                              </thead>
+														  <tbody>
         <?php
-                $motions=$db_con->prepare(
-                        "select * from motions where motion_disposition   NOT IN ('PASSED','FAILED','DEFERRED','REVOKED')");
-                $motions->execute(); 
-                while ( $row = $motions->fetch(PDO::FETCH_ASSOC))
-                { 
-			$motionid=$row['motion_id'];
-			$motionname=$row['motion_name'];
-			$dateadded=$row['dateadded'];		
-
-		echo '
-		<form id="vote" action="discussions.php" method="post">
-		<tr>
-                        	<td><input type="text" name="motionid" readonly value="'.$motionid. '" /> </td>
-				<td>' . $row['motion_name'].'</td>
-				<td>' . $row['dateadded'] . '</td>
-				<td><input type="submit" value="Submit"> 
+    $motions = $db_con->prepare("select * from motions where motion_disposition   NOT IN ('PASSED','FAILED','DEFERRED','REVOKED')");
+    $motions->execute();
+    while ($row = $motions->fetch(PDO::FETCH_ASSOC)) {
+        $motionid   = $row['motion_id'];
+        $motionname = $row['motion_name'];
+        $dateadded  = $row['dateadded'];
+        
+        echo '
+        <form id="vote" action="discussions.php" method="post">
+        <tr>
+                            <td><input type="text" name="motionid" readonly value="' . $motionid . '" /> </td>
+                <td>' . $row['motion_name'] . '</td>
+                <td>' . $row['dateadded'] . '</td>
+                <td><input type="submit" value="Submit"> 
                 </tr>
-		</form>';
-                }//end of while
-		echo '
-        	</table>';
-		$motions->closeCursor();
-	?>
-        <!-- /span6 -->
+        </form>';
+    } //end of while
+    echo '</tbody>
+    <tfoot>
+     <tr>
+                        <th>Motion ID</th>
+                        <th>Motion Name</th>
+                        <th>Date Added</th>
+            		<th>Action</th>
+                </tr>
+    </tfoot>
+            </table>';
+    $motions->closeCursor();
+?>
+       <!-- /span6 -->
         
         <!-- /span6 --> 
       </div>
@@ -264,6 +257,8 @@ $userid=$_SESSION['user_id'];
 
 <script src="js/bootstrap.js"></script>
  
-<?php } ?>
+<?php
+}
+?>
 </body>
 </html>
