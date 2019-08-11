082941040df4e78d7951cdc81b6a6c374aa326ee
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-08-10 22:36:03 -0400

    Update discussions.php

--- discussions.php
+++ discussions.php
@@ -61,6 +61,7 @@ $userid = $_SESSION['user_id'];
     <li><a href="vote.php"><em class ="icon-dashboard"></em><span>Vote</span></a></li>
      <li class="active"><a href="discussions.php"><em class ="icon-dashboard"></em><span>Discussions</span></a></li>
     <li><a href="userprefs.php"><em class ="icon-dashboard"></em><span>Prefences</span></a></li>
+    <li><a href="users.php"><em class="icon-dashboard"></em><span>Users</span></a></li>
          <li><a href="logout.php"><em class="icon-dashboard"></em><span>Logout</span></a> </li>
 
       </ul>
