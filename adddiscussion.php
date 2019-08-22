87402da77051f6d506996b6341bc4a663766cacc
Michael Brown <michaelbrown.tsbod@gmail.com>
2019-08-22 10:53:31 -0400

    Update adddiscussion.php

--- adddiscussion.php
+++ adddiscussion.php
@@ -60,7 +60,7 @@ header('location: index.php');
 					$body .= "<br />";
 					$body .= $emailDiscussions;
 				}//End of while
-
+					$body .= "Discussions can be viewed here:";
 				$body .= "</body>";
 				$body .= "</html>";
 			}
